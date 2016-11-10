3b22b56c8d62bbe8ba56e7194b3fd09352afc176
katniss_y <katniss_smile@sina.com>
2016-11-10 08:22:16 +0800

    ui

---
+++ ours/database_homework/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>图书馆空间预约系统</title>
+<style type="text/css">
+body {
+	background-color: #FFFFFF;
+}
+</style>
+</head>
+
+<body>
+<table width="1110" height="750" border="1" align="center">
+  <tr>
+    <td height="465" colspan="3" bgcolor="#FFFFFF"><img src="images/back.png" alt="back" width="1120" height="465" align="absmiddle" /></td>
+  </tr>
+  <tr>
+    <td width="370" height="285" bgcolor="#FFFFFF"><a href="student.php"><img src="images/student.png" width="370" height="280" alt="student" /></a></td>
+    <td width="370" bgcolor="#FFFFFF"><a href="team.php"><img src="images/team.png" alt="team" width="370" height="280" /></a></td>
+    <td width="370" bgcolor="#FFFFFF"><a href="worker.php"><img src="images/worker.png" width="370" height="280" alt="worker" /></a></td>
+  </tr>
+</table>
+<div align="center"></div>
+</body>
+</html>
---
+++ ours/database_homework/team.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>图书馆空间预约系统</title>
+</head>
+
+<body>
+<table width="1110" height="750" border="1" align="center">
+  <tr>
+    <td height="200" colspan="2">&nbsp;</td>
+  </tr>
+  <tr>
+    <td width="280">&nbsp;</td>
+    <td width="793">&nbsp;</td>
+  </tr>
+</table>
+
+</body>
+</html>
